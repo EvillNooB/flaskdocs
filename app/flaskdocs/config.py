7fff7ca616ac856c9da8579c5b0b70b919e39c62
Kairali Ashigaliyev <kairali@yandex.ru>
2020-05-05 15:18:26 +0500

    cleanup

--- app/flaskdocs/config.py
+++ app/flaskdocs/config.py
@@ -1,20 +1,25 @@
 import os 
+import json
+
+script_dir = os.path.dirname(__file__)
+rel_path = "config.json"
+abs_file_path = os.path.join(script_dir, rel_path)
+with open(abs_file_path) as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    TWILIO_AUTH_SID = os.environ.get('ACCOUNT_SSID')
-    TWILIO_AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
-    GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
-    GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
-    
-    TWILIO_FROM = '+12057518009'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    GOOGLE_DISCOVERY_URL = (
-        "https://accounts.google.com/.well-known/openid-configuration"
-                            )
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_USERNAME = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD = config.get('MAIL_PASSWORD')
+    TWILIO_AUTH_SID = config.get('TWILIO_AUTH_SID')
+    TWILIO_AUTH_TOKEN = config.get('TWILIO_AUTH_TOKEN')
+    GOOGLE_CLIENT_ID = config.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = config.get('GOOGLE_CLIENT_SECRET')
+
+    TWILIO_FROM = config.get('TWILIO_FROM')
+    SQLALCHEMY_TRACK_MODIFICATIONS = config.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    MAIL_SERVER = config.get('MAIL_SERVER')
+    MAIL_PORT = config.get('MAIL_PORT')
+    MAIL_USE_SSL = config.get('MAIL_USE_SSL')
+    GOOGLE_DISCOVERY_URL = config.get('GOOGLE_DISCOVERY_URL')
--- app/flaskdocs/main/utils.py
+++ app/flaskdocs/main/utils.py
@@ -3,7 +3,7 @@ from flask_mail import Message
 
 
 def send_email_to_staff(staff, document, daysleft):
-    msg = Message(f'Уведомление о документе - {document.name}', sender="kahrali.hvss@sas.ke", recipients=[staff.email])
+    msg = Message(f'Уведомление о документе - {document.name}', sender="noreply@dochub.info", recipients=[staff.email])
     msg.body = f'''{staff.first_name} {staff.second_name}, Ваш {document.name} истекает {document.expiration_date.format("DD.MM.YYYY")}
 Осталось дней {daysleft} 
 
@@ -17,7 +17,7 @@ def send_email_to_group(group, daysleft, document, staff):
     for user in group.user_count:
         if user.use_email:
             receivers.append(user.email)
-    msg = Message(f'Уведомление о документе - {document.name}', sender="kahrali.hvss@sas.ke", recipients=receivers)
+    msg = Message(f'Уведомление о документе - {document.name}', sender="noreply@dochub.info", recipients=receivers)
     msg.body = f'''Документ '{document.name}'' который принадлежит работнику - {staff.first_name} {staff.second_name} 
 Истекает {document.expiration_date.format("DD.MM.YYYY")}
 Осталось дней {daysleft} 
